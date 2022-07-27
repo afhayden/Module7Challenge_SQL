805a5eb6dcfe48d40265397e7a85a87ea8b22835
afhayden <100769036+afhayden@users.noreply.github.com>
2022-07-27 14:14:25 -0400

    Completed Deliverable 2

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -34,3 +34,22 @@ GROUP BY ut.title
 ORDER BY COUNT(*) DESC;
 
 SELECT * FROM retiring_titles
+
+
+-- Joining employees and titles tables to get mentor-eligible employees
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	 e.first_name,
+	 e.last_name,
+	 e.birth_date,
+	 ti.title,
+     de.from_date,
+     de.to_date
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	 AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
